cb12fccd86854a3f5325b79194630502c64df80e
ImAbinash <abinashpradhan38@gmail.com>
2021-01-16 12:55:37 +0530

    initial commit

---
+++ src/test/java/com/khub/springsecurity/SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khub.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
