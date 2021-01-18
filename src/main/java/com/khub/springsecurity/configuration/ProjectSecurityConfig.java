67a9baeeedb04c86d75218770232b01428a54183
ImAbinash <abinashpradhan38@gmail.com>
2021-01-18 08:22:50 +0530

    Implemented InMemoryUserDetailsManager.

--- src/main/java/com/khub/springsecurity/configuration/ProjectSecurityConfig.java
+++ src/main/java/com/khub/springsecurity/configuration/ProjectSecurityConfig.java
@@ -32,13 +32,13 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 	 * Usage: suppose wants to stop consuming the api's at mid night then we need to implement it.
 	 */
 	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-
-		http.authorizeRequests((requests) -> requests.anyRequest().denyAll());
-		http.formLogin();
-		http.httpBasic();
-	}
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//
+//		http.authorizeRequests((requests) -> requests.anyRequest().denyAll());
+//		http.formLogin();
+//		http.httpBasic();
+//	}
 	
 
 	/***
@@ -54,7 +54,31 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 //		http.httpBasic();
 //	}
 	
+	/**
+	 *  We used inMemoryAuthentication here to describe user and password with role.
+	 */
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.inMemoryAuthentication().withUser("test").password("test").authorities("admin").and().passwordEncoder(NoOpPasswordEncoder.getInstance());
+//	}
 	
 	
+	/**
+	 * Here we used InMemoryUserDetailsManager to manage user details
+	 */
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+		UserDetails user1 = User.withUsername("admin").password("password").authorities("admin").build();
+		UserDetails user2 = User.withUsername("user").password("password").authorities("admin").build();
+		userDetailsService.createUser(user1);
+		userDetailsService.createUser(user2);
+		auth.userDetailsService(userDetailsService);
+		
+	}
 	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
 }
